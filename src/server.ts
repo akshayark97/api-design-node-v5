1a88be71a1568b8323bba1d69821cdb27b64a9d3
Akshay R K <akshayrk@Akshays-Mac-mini.local>
2026-05-28 22:43:12 +0530

    Requests and Responses

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,21 @@ import express from 'express'
 const app = express()
 
 app.get('/health', (req, res) => {
-    res.send('<h1>Server is healthy</h1>')
+  res.send('<h1>Server is healthy</h1>')
 })
 
-export  { app }
+app.post('/cake', (req, res) => {
+  res.send('Ok')
+})
+
+app.post('/cake/:id', (req, res) => {
+  res.send(`Send cake to me with ID: ${req.params.id}`)
+})
+
+app.post("/cake/:name/:id", (req, res) => {
+    res.send("Send the cake to me with name: " + req.params.name + " and ID: " + req.params.id)
+})
+
+export { app }
 
 export default app
