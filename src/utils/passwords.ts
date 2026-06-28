0529606b6ef037cb6cf639be75e176a7bea3c5cc
Akshay R K <akshayrk@Akshays-Mac-mini.local>
2026-06-28 16:40:44 +0530

    Sign-In Controller

--- src/utils/passwords.ts
+++ src/utils/passwords.ts
@@ -2,5 +2,12 @@ import bcrypt from 'bcrypt'
 import env from '../../env.ts'
 
 export const hashPassword = async (password: string) => {
-    return bcrypt.hash(password, env.BCRYPT_ROUNDS)
+  return bcrypt.hash(password, env.BCRYPT_ROUNDS)
+}
+
+export const comparePassword = async (
+  password: string,
+  hashedPassword: string,
+) => {
+  return bcrypt.compare(password, hashedPassword)
 }
