44e98efce879bb344392add59c04ca4b2aa2d004
Akshay R K <akshayrk@Akshays-Mac-mini.local>
2026-07-02 23:07:34 +0530

    Fix unit test fail in auth controller

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -67,20 +67,18 @@ export const login = async (req: Request, res: Response) => {
       username: user.username,
     })
 
-    return res
-      .json({
-        message: 'Login success',
-        user: {
-          id: user.id,
-          email: user.email,
-          username: user.username,
-          firstName: user.firstName,
-          lastName: user.lastName,
-          createdAt: user.createdAt,
-        },
-        token,
-      })
-      .status(201)
+    return res.status(201).json({
+      message: 'Login success',
+      user: {
+        id: user.id,
+        email: user.email,
+        username: user.username,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        createdAt: user.createdAt,
+      },
+      token,
+    })
   } catch (e) {
     console.error('Login error', e)
     res.status(500).json({ error: 'Failed to login' })
